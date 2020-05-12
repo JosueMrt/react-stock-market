bfbc9d9ee0eaa4e3077dd1b585d8c6811e59814e
axe i-grec <tag4rce7@gmail.com>
2020-05-12 11:00:24 +0200

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Chart } from "./Chart/Chart";
+export { default as Info } from "./Info/Info";
+export { default as News } from "./News/News";
+export { default as SearchBar } from "./SearchBar/SearchBar";
