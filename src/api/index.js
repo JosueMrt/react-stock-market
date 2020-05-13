8d9ca7c262bf8eec3e695aa0bf37cb5e3235ffe8
axe i-grec <tag4rce7@gmail.com>
2020-05-13 16:28:47 +0200

    Added chart

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,12 @@
-export const fetchData = async (url) => {
-  const response = await fetch(url);
+export const fetchDailyData = async (ticker = "spy") => {
+  const response = await fetch(
+    "https://sandbox.iexapis.com/stable/stock/" +
+      ticker +
+      "/chart?token=Tsk_536fd841bd3446e09e6a7895835fa6c6"
+  );
+  console.log("feteched");
   const data = await response.json();
-  return data;
+  return data.map((val, i) => {
+    return { date: data[i].date, close: data[i].close };
+  });
 };
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React from "react";
+import { fetchDailyData } from "../../api";
+import { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
 const Chart = () => {
-    return (
-        <div>Chart</div>
-    );
-}
- 
-export default Chart
+  const [dailyData, setDailyData] = useState([]);
+  useEffect(() => {
+    const fetch = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    fetch();
+  }, []);
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ close }) => close),
+            label: "Close Price",
+            borderColor: "#333ff",
+            backgroundColor: "#00c805",
+            fill: true,
+            lineTension: 0.1,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  return <div className={styles.container}>{lineChart}</div>;
+};
+
+export default Chart;
