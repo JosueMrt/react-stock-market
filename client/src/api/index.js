3be8824552f417f603137a84a01cea344728ebfb
axe i-grec <tag4rce7@gmail.com>
2020-05-20 18:44:02 +0200

    Setup backend for private api calls

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,5 @@
 export const fetchDailyData = async (ticker, time) => {
-  const response = await fetch(
-    `https://sandbox.iexapis.com/stable/stock/${ticker}/chart/${time}?token=Tsk_536fd841bd3446e09e6a7895835fa6c6`
-  );
+  const response = await fetch(`/api/dailydata/${ticker}/${time}`);
   console.log("fetched: " + ticker);
   const data = await response.json();
   return data.map((val, i) => {
@@ -10,33 +8,25 @@ export const fetchDailyData = async (ticker, time) => {
 };
 
 export const fetchInfo = async (ticker) => {
-  const response = await fetch(
-    `https://sandbox.iexapis.com/stable/stock/${ticker}/company?token=Tsk_536fd841bd3446e09e6a7895835fa6c6`
-  );
+  const response = await fetch(`/api/info/${ticker}`);
   const { symbol, companyName } = await response.json();
   return { symbol, companyName };
 };
 
 export const fetchLogo = async (ticker) => {
-  const response = await fetch(
-    `https://cloud.iexapis.com/stable/stock/${ticker}/logo?token=pk_bc14b78153d24d7d878689bf51c9360d `
-  );
+  const response = await fetch(`/api/logo/${ticker}`);
   const { url } = await response.json();
   return url;
 };
 
 export const fetchCurrentPrice = async (ticker) => {
-  const response = await fetch(
-    `https://sandbox.iexapis.com/stable/stock/${ticker}/price?token=Tsk_536fd841bd3446e09e6a7895835fa6c6`
-  );
+  const response = await fetch(`/api/price/${ticker}`);
   const data = await response.json();
   return data;
 };
 
 export const fetchNews = async (ticker) => {
-  const response = await fetch(
-    `https://cloud.iexapis.com/stable/stock/${ticker}/news/last/3?token=pk_bc14b78153d24d7d878689bf51c9360d`
-  );
+  const response = await fetch(`/api/news/${ticker}`);
   const data = await response.json();
   return data;
 };
---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+const app = express();
+
+const api_key = process.env.API_KEY;
+const sb_api_key = process.env.SB_API_KEY;
+
+app.get("/api/dailydata/:ticker/:time", async (req, res) => {
+  const { ticker, time } = req.params;
+  const fetch_response = await fetch(
+    `https://sandbox.iexapis.com/stable/stock/${ticker}/chart/${time}?token=${sb_api_key}`
+  );
+  const data = await fetch_response.json();
+  res.json(data);
+});
+
+app.get("/api/info/:ticker", async (req, res) => {
+  const { ticker } = req.params;
+  const response = await fetch(
+    `https://sandbox.iexapis.com/stable/stock/${ticker}/company?token=${sb_api_key}`
+  );
+  const data = await response.json();
+  res.json(data);
+});
+
+app.get("/api/logo/:ticker", async (req, res) => {
+  const { ticker } = req.params;
+  const response = await fetch(
+    `https://cloud.iexapis.com/stable/stock/${ticker}/logo?token=${api_key}`
+  );
+  const data = await response.json();
+  res.json(data);
+});
+
+app.get("/api/price/:ticker", async (req, res) => {
+  const { ticker } = req.params;
+  const response = await fetch(
+    `https://sandbox.iexapis.com/stable/stock/${ticker}/price?token=${sb_api_key}`
+  );
+  const data = await response.json();
+  res.json(data);
+});
+
+app.get("/api/news/:ticker", async (req, res) => {
+  const { ticker } = req.params;
+  const response = await fetch(
+    `https://cloud.iexapis.com/stable/stock/${ticker}/news/last/3?token=pk_bc14b78153d24d7d878689bf51c9360d`
+  );
+  const data = await response.json();
+  res.json(data);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => `Server running on port ${port}`);
