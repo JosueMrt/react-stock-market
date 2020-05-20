3c444f8764338e92566990e53879d82543b042b4
axe i-grec <tag4rce7@gmail.com>
2020-05-20 22:16:49 +0200

    Small API Call bug fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require('path')
+const path = require("path");
 const fetch = require("node-fetch");
 require("dotenv").config();
 
@@ -7,11 +7,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, "client/build")));
 
 const api_key = process.env.API_KEY;
+const sb_api_key = process.env.SB_API_KEY;
 
 app.get("/api/dailydata/:ticker/:time", async (req, res) => {
   const { ticker, time } = req.params;
   const fetch_response = await fetch(
-    `https://cloud.iexapis.com/stable/stock/${ticker}/chart/${time}?token=${api_key}`
+    `https://sandbox.iexapis.com/stable/stock/${ticker}/chart/${time}?token=${sb_api_key}`
   );
   const data = await fetch_response.json();
   res.json(data);
@@ -20,7 +21,7 @@ app.get("/api/dailydata/:ticker/:time", async (req, res) => {
 app.get("/api/info/:ticker", async (req, res) => {
   const { ticker } = req.params;
   const response = await fetch(
-    `https://cloud.iexapis.com/stable/stock/${ticker}/company?token=${api_key}`
+    `https://sandbox.iexapis.com/stable/stock/${ticker}/company?token=${sb_api_key}`
   );
   const data = await response.json();
   res.json(data);
@@ -38,7 +39,7 @@ app.get("/api/logo/:ticker", async (req, res) => {
 app.get("/api/price/:ticker", async (req, res) => {
   const { ticker } = req.params;
   const response = await fetch(
-    `https://cloud.iexapis.com/stable/stock/${ticker}/price?token=${api_key}`
+    `https://sandbox.iexapis.com/stable/stock/${ticker}/price?token=${sb_api_key}`
   );
   const data = await response.json();
   res.json(data);
